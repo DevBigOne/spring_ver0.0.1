ed16d40e258262bd551e9d4320875f5c884e55f0
김대원 <a62387272@gmail.com>
2024-03-01 16:12:59 +0900

    Spring 회원 등록 공부

--- hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,11 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -11,6 +14,20 @@ public class MemberController {
     @Autowired //스프링이 컨테이너에 있는 멤버서비스를 연결해줌
     public MemberController(MemberService memberService){
         this.memberService = memberService;
+    }
+    @GetMapping("/members/new") // 조회할 때 사용 됨
+    public String createForm() {
+        return "members/createMemberForm"; //createMemberForm으로 이동하는 메소드
+    }
+
+    @PostMapping("members/new") //데이터 전달할 때 post 사용
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName()); // 등록된 회원이름 꺼냄.
+
+        memberService.join(member);
+
+        return "redirect:/";
     }
 
 }
